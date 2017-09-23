9779318f11bb68d60b8498c4d88bd94e804cf9fc
Trevor Vaughn <TrevorJVaughn@gmail.com>
2017-09-22 17:54:21 -1000

    UI updates, engine updates, REady to send mac

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public enum GameState { PreGame, Playing, Dead, Win };
     public static GameState gameState;
 
-    public GameObject gameStartText, deadText, winText;
+    public GameObject gameStartText, deadText, winText, controlText;
+    Image controlsImage;
+    bool isFadingControlsImage;
 
     public bool canRestart;
 
 	// Use this for initialization
 	void Start ()
     {
+        isFadingControlsImage = false;
+        controlsImage = controlText.GetComponent<Image>();
         gameState = GameState.PreGame;
         canRestart = false;
 	}
@@ -21,7 +26,15 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	private void Update ()
     {
-
+        if (isFadingControlsImage)
+        {
+            if (controlsImage.color.a > 0)
+            {
+                Color c = controlsImage.color;
+                c.a -= (2.5f * Time.deltaTime);
+                controlsImage.color = c;
+            }
+        }
 	}
 
     public void ChangeUIFromPreToPlaying()
@@ -29,6 +42,8 @@ public class GameManager : MonoBehaviour {
         gameStartText.SetActive(false);
         deadText.SetActive(false);
         winText.SetActive(false);
+        controlText.SetActive(true);
+        StartCoroutine(FadeControlsImage());
     }
 
     public void ChangeUIFromPlayingToWin()
@@ -36,6 +51,7 @@ public class GameManager : MonoBehaviour {
         gameStartText.SetActive(false);
         deadText.SetActive(false);
         winText.SetActive(true);
+        controlText.SetActive(false);
     }
 
     public void ChangeUIFromPlayingToDead()
@@ -43,6 +59,7 @@ public class GameManager : MonoBehaviour {
         gameStartText.SetActive(false);
         deadText.SetActive(true);
         winText.SetActive(false);
+        controlText.SetActive(false);
     }
 
     IEnumerator respawnTimer()
@@ -50,5 +67,11 @@ public class GameManager : MonoBehaviour {
         yield return new WaitForSeconds(1.5f);
         deadText.SetActive(true);
         canRestart = true;
+    }
+
+    IEnumerator FadeControlsImage()
+    {
+        yield return new WaitForSeconds(6);
+        isFadingControlsImage = true;
     }
 }
